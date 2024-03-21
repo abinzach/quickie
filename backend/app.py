1fa5ef13a41efa35fd85f667c6407da62703db8e
abinzach <abinzacharia1998@gmail.com>
2024-03-21 14:57:39 +0530

    backend change port

--- backend/app.py
+++ backend/app.py
@@ -42,4 +42,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)  # You may want to set debug to False in production
+    app.run(port='0.0.0.0',debug=False)  # You may want to set debug to False in production
